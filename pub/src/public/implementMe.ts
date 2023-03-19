d001e4c29912f2ad24ccdb2ec8320bb1ceef3acb
corno <corno.astn.a@schraverus.com>
2023-03-19 08:53:48 +0100

    .

--- pub/src/public/implementMe.ts
+++ pub/src/public/implementMe.ts
@@ -1,3 +1,8 @@
-export function implementMe(message: string): never {
-    throw new Error(`IMPLEMENT ME: ${message}`)
+import * as pi from 'pareto-core-internals'
+
+export function implementMe() {
+    console.warn(`${pi.getLocationInfo(1)}: 'IMPLEMENT ME' statement`)
+    return (message: string): never => {
+        throw new Error(`IMPLEMENT ME: ${message}`)
+    }
 }
--- pub/src/public/logDebugMessage.ts
+++ pub/src/public/logDebugMessage.ts
@@ -1,3 +1,9 @@
-export function logDebugMessage(message: string) {
-    console.log("DEBUG", message)
+import * as pi from 'pareto-core-internals'
+
+export function logDebugMessage() {
+
+    console.warn(`${pi.getLocationInfo(1)}: 'IMPLEMENT ME' statement`)
+    return (message: string) => {
+        console.log("DEBUG", message)
+    }
 }
