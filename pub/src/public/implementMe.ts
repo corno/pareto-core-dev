814ffa37b16859c634419f353b234139257c63ea
corno <corno.astn.a@schraverus.com>
2023-03-19 14:34:02 +0100

    Revert "."
    This reverts commit d001e4c29912f2ad24ccdb2ec8320bb1ceef3acb.

--- pub/src/public/implementMe.ts
+++ pub/src/public/implementMe.ts
@@ -1,8 +1,3 @@
-import * as pi from 'pareto-core-internals'
-
-export function implementMe() {
-    console.warn(`${pi.getLocationInfo(1)}: 'IMPLEMENT ME' statement`)
-    return (message: string): never => {
-        throw new Error(`IMPLEMENT ME: ${message}`)
-    }
+export function implementMe(message: string): never {
+    throw new Error(`IMPLEMENT ME: ${message}`)
 }
--- pub/src/public/logDebugMessage.ts
+++ pub/src/public/logDebugMessage.ts
@@ -1,9 +1,3 @@
-import * as pi from 'pareto-core-internals'
-
-export function logDebugMessage() {
-
-    console.warn(`${pi.getLocationInfo(1)}: 'IMPLEMENT ME' statement`)
-    return (message: string) => {
-        console.log("DEBUG", message)
-    }
+export function logDebugMessage(message: string) {
+    console.log("DEBUG", message)
 }
