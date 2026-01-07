ccde095c0b12ecd874f74c08a6cee81d16b1dfad
Corno Schraverus <corno@schraverus.com>
2026-01-07 18:20:31 +0100

    ...

--- pub/src/public/log_wrapping_debug_messages.ts
+++ pub/src/public/log_wrapping_debug_messages.ts
@@ -1,19 +1,15 @@
 
-/**
- * logs a debug message
- * this function should only be called from code that is in the development phase,
- * before publishing, the usages should be removed
- * @param message the string to be printed to stdout
- */
+
 export function log_wrapping_debug_messages<T>(
     message: string,
     handler: () => T,
 ): T {
+    console.log()
     console.log("DEBUG BEFORE", message)
     const startTime = Date.now()
     const x = handler()
     const duration = Date.now() - startTime
-    console.log("DEBUG AFTER", message, `(${duration}ms)`)
+    console.log("DEBUG AFTER", message.concat(' '.repeat(Math.max(0, 20 - message.length))), `(${duration}ms)`)
     return x
 
 }
