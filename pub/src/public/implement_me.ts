87a6b9077b5e74fcc6f799b4c596a7147a8f7a49
Corno Schraverus <corno@schraverus.com>
2026-01-22 09:26:33 +0100

    .

--- pub/src/public/implement_me.ts
+++ pub/src/public/implement_me.ts
@@ -1,7 +1,7 @@
 
 import {
     $$ as get_location_info,
-} from "pareto-core-internals/dist/__internals/sync/get_location_info"
+} from "pareto-core/dist/__internals/sync/get_location_info"
 
 /**
  * use this function as a placeholder when you want to compile but have not fully developed all functionality yet.
